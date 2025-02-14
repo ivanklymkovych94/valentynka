fe2113bade77b2374bd548ac37926e90df182cb7
Ivan <149456927+ivanklymkovych94@users.noreply.github.com>
2025-02-14 06:43:09 +0200

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const card = document.querySelector(".card");
+    const container = document.querySelector(".container");
 
-    document.querySelector(".container").addEventListener("mouseenter", () => card.style.top = "-90px");
-    document.querySelector(".container").addEventListener("mouseleave", () => card.style.top = "0");
-    document.querySelector(".container").addEventListener("click", () => card.style.top = "-90px");
-    document.body.addEventListener("click", () => card.style.top = "0", true);
 
     card.style.transition = "top 0.5s";
+
+ 
+    const showCard = () => card.style.top = "-90px";
+    
+
+    const hideCard = () => card.style.top = "0";
+
+  
+    container.addEventListener("mouseenter", showCard);
+    container.addEventListener("mouseleave", hideCard);
+    container.addEventListener("click", showCard);
+    document.body.addEventListener("click", hideCard, true);
+
+
+    container.addEventListener("touchstart", showCard);
+    document.body.addEventListener("touchend", hideCard, true);
 });
